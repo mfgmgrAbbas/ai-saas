353988769650b6ace9444350b43cdc9d3036c68d
mfgmgrAbbas <smasnb12@gmail.com>
2023-07-24 14:01:44 +0500

    initial

---
+++ components/sidebar.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+
+import React from 'react'
+
+const Sideabr = () => {
+  return (
+    <div className='space-y-4 py-4 flex flex-col backdrop:h-full 
+    bg-[#111827] text-white '>
+        
+        Hello Sideabr
+
+    </div>
+  )
+}
+
+export default Sideabr

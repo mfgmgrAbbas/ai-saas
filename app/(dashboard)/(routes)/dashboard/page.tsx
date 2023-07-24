cb59c63ddaa6d484a75a48043e3cb776e112997d
mfgmgrAbbas <smasnb12@gmail.com>
2023-07-24 13:01:05 +0500

    initial

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+
+
+
+
+const DashboardPage = () => {
+    return (
+        <div>
+        <p>Dashboard Page (Protected)</p>
+     
+        </div>
+            )
+}
+
+export default DashboardPage
